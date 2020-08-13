a855c6aa81584209fdcd6bb793e96dbbb1975bc0
wnsdud4206 <wnsdud4206@gmail.com>
2020-08-13 20:19:40 +0900

    20200813

--- syntax/template.js
+++ syntax/template.js
@@ -7,7 +7,7 @@ console.log(letter);
 // ``안에(esc 버튼 아래, 그레이브 엑센트) ${변수}를 삽입하면 변수 내용 적용 : tamplate literal(template string)
 var letter = `Lorem ${name}
 
-ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ${name} aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ${name}`;
+ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis ${name} aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ${name} ${1+1}`;
 console.log(letter);
 
 var a = 1;
